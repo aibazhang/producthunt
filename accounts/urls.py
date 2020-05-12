ebd16ee61dbe88573fc2a44dde18b5cefaa0a3ec
aibazhang <aiba_hz@hotmail.com>
2020-05-13 00:11:11 +0900

    add function of signup & login

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('sign_up/', views.sign_up, name='sing_up'),
+    path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
